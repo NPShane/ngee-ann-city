2c52877a415d4826c0969f1afe46fdd531e9e155
Ignatius Zhi Wei Joshua Hee <s10247763@connect.np.edu.sg>
2023-12-08 01:27:56 +0800

    Implemented the display of the board and menus

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 game_vars = {
@@ -40,6 +41,88 @@ def show_main_menu():
     option = get_input(4)
     return option
 
+field = [ [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None] ]
+
+def draw_field():
+    num_rows = len(field)
+    num_cols = len(field[0])
+    # Print column numbers and top line
+    print('     ')
+    for i in range(len(field[0])):
+        print(f'{i + 1:6}', end='')
+    print('\n  +' + '-----+' * num_cols)
+    # Print each row
+    for row_num in range(num_rows):
+        # Print line that shows names and row alphabets
+        print(f" {chr(row_num+65)}|",end='')
+        for col_num in range(num_cols):
+            print('     |',end='')
+        # Print line that shows unit health
+        print(f"\n  |",end='')
+        for col_num in range(num_cols):
+            print('     |',end='')
+        # Close off row with last line
+        print('\n  +' + '-----+' * num_cols)
+
+    print("  Coins: " + "Points: " + "Turn: ")
+
+    await_user()
+
+    show_building_types()
+
+    await_user()
+
+
+def show_building_types():
+    # Print options
+    print("Build a Building - Building Types Available")
+    print("-------------------------------------------")
+    print("Commercial - Cost: 1 coin")
+    print("Industry - Cost: 1 coin")
+    print("Park - Cost: 1 coin")
+    print("Residential - Cost: 1 coin")
+    print("Road - Cost: 1 coin")
+
+
+def building_given():
+    # Randomly pick 2 out of 5 building types (Not implemented yet)
+    print("You have been offered")
+    print("---------------------")
+    print("[1]")
+    print("[2]")
+    print()
+    print("Choose your option (1 or 2):")
+    option = get_input(2)
+    return option
+
+
+def show_building_menu(game_vars):
+    # Print options
+    print("1. Buy unit     2. End turn     3. Upgrade unit")
+    print("4. Save game    5. Quit")
+    # Get what user wants to do
+    option = get_input(5)
+    return option
+
 
 def show_high_scores():
     try:
@@ -74,7 +157,7 @@ if __name__ == "__main__":
             main_option = show_main_menu()
 
             if main_option == 1:
-                game_playing = not game_playing
+                game_playing = draw_field()
                 # TODO: initialise game
             elif main_option == 2:  # TODO: add load_game(game_vars) to this condition later
                 game_playing = not game_playing
@@ -86,3 +169,104 @@ if __name__ == "__main__":
         while not game_vars["game_over"]:
             # TODO: implement turn-to-turn gameplay logic
             pass
+
+
+#-----------------------------------------
+# save_game()
+#
+#    Saves the game in the file 'save.txt'
+#-----------------------------------------
+#def save_game():
+    #file = open('save.txt', 'w')
+    # Store game_vars 
+    #for var in game_vars.values():
+        #file.write(str(var))
+        #file.write(',')
+    #file.write('\n\n')
+    # Then store field
+    #for row in field:
+        #for unit in row:
+            #if unit:
+                #for attribute in unit.values():
+                    #file.write(str(attribute))
+                    #file.write(',')
+            #else:
+                #file.write('None')
+            #file.write(';')
+        #file.write('\n')
+    #file.write('\n')
+    # Then save player settings
+    #for setting in player_defined_vars.values():
+        #file.write(str(setting[0]))
+        #file.write(',')
+    #print("Game saved.")
+    #file.close()
+
+#-----------------------------------------
+# load_game()
+#
+#    Loads the game from 'save.txt'
+#-----------------------------------------
+#def load_game(game_vars):
+    #try:
+        #file = open('save.txt')
+    #except FileNotFoundError:
+        #print('No save file to load from.')
+        #return False
+    #file_list = file.read().split('\n\n')
+    #setting_list = file_list.pop().split(',')[:-1]
+    #field_list = file_list.pop().split('\n')
+    #var_list = file_list.pop().split(',')[:-1]
+    # Load in game_vars from save
+    #count = 0
+    #for var in game_vars:
+        #if var_list[count].isdigit():
+            #game_vars[var] = int(var_list[count])
+        #elif var_list[count] == 'False':
+            #game_vars[var] = False
+        #elif var_list[count] == 'True':
+            #game_vars[var] = True 
+        #count += 1
+    # Ready vars for use
+    #game_vars['turn'] -= 1
+    #for i in range(game_vars['danger_level']-1):
+        #for monster in monsters.values():
+            #monster['maxHP'] += 1
+            #monster['min_damage'] += 1
+            #monster['max_damage'] += 1
+            #monster['reward'] += 1
+    # Load in player settings and change other stuff using them
+    #count = 0
+    #for var in player_defined_vars:
+        #player_defined_vars[var][0] = int(setting_list[count])
+        #count += 1
+    #global field
+    #field = []
+    #for row in range(player_defined_vars['board_width'][0]):
+        #field.append([])
+        #for column in range(player_defined_vars['board_length'][0]):
+            #field[row].append(None)
+    # Load in field from save
+    #for row_num in range(len(field_list)):
+        #load_row = field_list[row_num].split(';')[:-1]
+        #for col_num in range(len(load_row)):
+            #load_space = load_row[col_num].split(',')
+            #if load_space[0] != 'None':
+                #unit = {}
+                #unit['name'] = load_space[0]
+                #unit['short_name'] = load_space[1]
+                #unit['maxHP'] = int(load_space[2])
+                #unit['HP'] = int(load_space[3])
+                #unit['min_damage'] = int(load_space[4])
+                #unit['max_damage'] = int(load_space[5])
+                #if unit['short_name'] in defenders:
+                    #unit['price'] = int(load_space[6])
+                    #if unit['short_name'] == 'CANON':
+                        #unit['fire_cycle'] = load_space[7]
+                #else:
+                    #unit['reward'] = int(load_space[6])
+                #field[row_num][col_num] = unit
+            #else:
+                #field[row_num][col_num] = None
+    #file.close()
+    #return True
